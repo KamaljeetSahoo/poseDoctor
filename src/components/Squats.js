c6dd597c3a87c9b6c168988caea1527ac449aed7
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-03-15 17:52:14 +0530

    sqaut page

--- src/components/Squats.js
+++ src/components/Squats.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { Col, Row } from 'reactstrap'
+import Webcam from 'react-webcam'
 
 const Squats = () => {
+    const webcamRef = useRef(0)
+    const canvasRef = useRef(0)
   return (
-    <div>Squats</div>
+    <div>
+        <Row>
+            <Col md={6}>
+                <div className='align-items-center justify-content-center'>
+									<Webcam
+									ref={webcamRef}
+									style={{
+											position: "absolute",
+											marginLeft: "0px",
+											marginRight: "0px",
+											textAlign: "center",
+											zindex: 9,
+											width: "400",
+											height: 'auto',
+											marginBottom: "0px",
+									}} />
+									<canvas
+									ref={canvasRef}
+									style={{
+											position: "absolute",
+											marginLeft: "0px",
+											marginRight: "0px",
+											textAlign: "center",
+											zindex: 9,
+											width: "400",
+											height: 'auto',
+											marginBottom: "0px"
+									}}>
+									</canvas>
+                </div>
+            </Col>
+            <Col md={6} style={{position:'relative'}}>Hello</Col>
+        </Row>
+				<Row>
+					Hello
+				</Row>
+    </div>
   )
 }
 
