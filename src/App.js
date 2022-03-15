b1823cc6b8d27daae6e23833ddf032ae8d1cf6f0
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-03-16 01:33:34 +0530

    reset timer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import Home from "./components/Home";
 import HandExtension from "./components/HandExtension";
 import NavbarComp from "./components/UI_Components/Navbar";
 import MediaPipeComp from "./components/MediaPipeComp";
-<<<<<<< HEAD
 import RightHandExtension from './components/RightHandExtension'
 import Timer from './components/timer'
-=======
-import RightHandExtension from './components/RightHandExtension';
 import Lunges from './components/Lunges';
 import AromFlexion from './components/AromFlexion';
 import AromLateralFlexion from './components/AromLateralFlexion';
 import ShoulderExtension from './components/ShoulderExtension';
->>>>>>> 3bf4791658a84bdc066960536f0e716c57862bab
 
 function App() {
   return (
@@ -28,14 +24,11 @@ function App() {
               <Route exact path='/handExtension' element={<HandExtension/>}/>
               <Route exact path='/demo' element={<MediaPipeComp/>}/>
               <Route exact path='/rightHandExtension' element={<RightHandExtension/>}/>
-<<<<<<< HEAD
               <Route exact path='/timer' element={<Timer/>}/>
-=======
               <Route exact path='/lunges' element={<Lunges/>}/>
               <Route exact path='/arom_flexion' element={<AromFlexion/>}/>
               <Route exact path='/arom_lateral_flexion' element={<AromLateralFlexion/>}/>
               <Route exact path='/shoulderExtension' element={<ShoulderExtension/>}/>
->>>>>>> 3bf4791658a84bdc066960536f0e716c57862bab
             </Routes>
           </div>
         </div>
--- src/components/RightHandExtension.js
+++ src/components/RightHandExtension.js
@@ -1,132 +1,123 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Pose, POSE_CONNECTIONS } from '@mediapipe/pose'
-import * as cam from '@mediapipe/camera_utils'
-import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils'
-import Webcam from 'react-webcam'
-import {Doughnut} from 'react-chartjs-2';
+import React, { useEffect, useRef, useState } from "react";
+import { Pose, POSE_CONNECTIONS } from "@mediapipe/pose";
+import * as cam from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import Webcam from "react-webcam";
+import { Doughnut } from "react-chartjs-2";
+import { joints } from "./Joints";
+import CanvasWebCam from "./UI_Components/CanvasWebCam";
 
-import { joints } from './Joints'
-import { PieChart, Pie} from 'recharts';
 var count = 0;
 var mode = null;
-var adhere = 10
-var camera = null
-var message = "start excercise"
+var adhere = 10;
+var camera = null;
+var message = "start excercise";
 const RightHandExtension = () => {
-    const data = {
-        labels: ['Adherence','rem'],
-        datasets: [
-            {
-                label: 'ADHERENCE',
-                data: [count,adhere-count],
-                borderColor: ['rgba(255,206,86,0.2)'],
-                backgroundColor: ['rgba(232,99,132,1)',
-                'rgba(232,211,6,1)',
-                 ],
-                pointBackgroundColor: 'rgba(255,206,86,0.2)',
-            }
-            
-        ]
-    }
-    const options = {
+  const data = {
+    labels: ["Adherence", "rem"],
+    datasets: [
+      {
+        label: "ADHERENCE",
+        data: [count, adhere - count],
+        borderColor: ["rgba(255,206,86,0.2)"],
+        backgroundColor: ["rgba(232,99,132,1)", "rgba(232,211,6,1)"],
+        pointBackgroundColor: "rgba(255,206,86,0.2)",
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "Doughnut Chart",
+        color: "blue",
+        font: {
+          size: 18,
+        },
         responsive: true,
-        maintainAspectRatio: true,
-        plugins: {
-            title: {
-                display: true,
-                text: 'Doughnut Chart',
-                color:'blue',
-                font: {
-                    size:18
-                },
-                responsive:true,
-                animation:{
-                    animateScale: true,
-                               }
-            }
-        }
-        
-    }
-    
-    // const data = [
-    //     {name: 'Geeksforgeeks', students: 400},
-    //     {name: 'Technical scripter', students1: 700},
-    //     // {name: 'Geek-i-knack', students: 200},
-    //     // {name: 'Geek-o-mania', students: 1000}
-    //     ];
-  const webcamRef = useRef(0)
-  const canvasRef = useRef(0)
-  const Ref = useRef(null)
+        animation: {
+          animateScale: true,
+        },
+      },
+    },
+  };
+
+  // const data = [
+  //     {name: 'Geeksforgeeks', students: 400},
+  //     {name: 'Technical scripter', students1: 700},
+  //     // {name: 'Geek-i-knack', students: 200},
+  //     // {name: 'Geek-o-mania', students: 1000}
+  //     ];
+  const webcamRef = useRef(0);
+  const canvasRef = useRef(0);
+  const Ref = useRef(null);
 
   // The state for our timer
-  const [timer, setTimer] = useState('00:00:00');
+  const [timer, setTimer] = useState("00:00:00");
   const getTimeRemaining = (e) => {
     const total = Date.parse(e) - Date.parse(new Date());
     const seconds = Math.floor((total / 1000) % 60);
     const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / 1000 * 60 * 60) % 24);
+    const hours = Math.floor(((total / 1000) * 60 * 60) % 24);
     return {
-        total, hours, minutes, seconds
+      total,
+      hours,
+      minutes,
+      seconds,
     };
-}
-const startTimer = (e) => {
-    
-    let { total, hours, minutes, seconds }
-                = getTimeRemaining(e);
-    
+  };
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+
     if (total >= 0) {
-
-        // update the timer
-        // check if less than 10 then we need to
-        // add '0' at the begining of the variable
-        setTimer(
-            (hours > 19 ? hours : '0' + hours) + ':' +
-            (minutes > 19 ? minutes : '0' + minutes) + ':'
-            + (seconds > 19 ? seconds : '0' + seconds)
-        )
+      // update the timer
+      // check if less than 10 then we need to
+      // add '0' at the begining of the variable
+      setTimer(
+        (hours > 19 ? hours : "0" + hours) +
+          ":" +
+          (minutes > 19 ? minutes : "0" + minutes) +
+          ":" +
+          (seconds > 19 ? seconds : "0" + seconds)
+      );
     }
-}
-const clearTimer = (e) => {
-
+  };
+  const clearTimer = (e) => {
     // If you adjust it you should also need to
     // adjust the Endtime formula we are about
-    // to code next	
-    setTimer('00:00:20');
+    // to code next
+    setTimer("00:00:20");
 
     // If you try to remove this line the
     // updating of timer Variable will be
     // after 1000ms or 1sec
     if (Ref.current) clearInterval(Ref.current);
     const id = setInterval(() => {
-        startTimer(e);
-    }, 1000)
+      startTimer(e);
+    }, 1000);
     Ref.current = id;
-}
+  };
 
-const getDeadTime = () => {
+  const getDeadTime = () => {
     let deadline = new Date();
 
     // This is where you need to adjust if
     // you entend to add more time
     deadline.setSeconds(deadline.getSeconds() + 20);
     return deadline;
-}
-
-
-
-  
+  };
 
   function calculateAngles(a, b, c) {
+    var radians =
+      Math.atan2(c[1] - b[1], c[0] - b[0]) -
+      Math.atan2(a[1] - b[1], a[0] - b[0]);
+    var angle = Math.abs((radians * 180.0) / Math.PI);
 
-    var radians = Math.atan2(c[1] - b[1], c[0] - b[0]) - Math.atan2(a[1] - b[1], a[0] - b[0]);
-    var angle = Math.abs(radians * 180.0 / Math.PI);
-
-    if (angle > 180.0)
-      angle = 360 - angle;
+    if (angle > 180.0) angle = 360 - angle;
     return angle;
   }
-
-
 
   function poseEstimation(results) {
     const canvasElement = canvasRef.current;
@@ -137,23 +128,39 @@ const getDeadTime = () => {
     var height = canvasElement.height;
 
     if (results.poseLandmarks) {
-
       var first_joint = {
         name: "right_shoulder",
-        coord: [results.poseLandmarks[joints.right_shoulder].x, results.poseLandmarks[joints.right_shoulder].y]
+        coord: [
+          results.poseLandmarks[joints.right_shoulder].x,
+          results.poseLandmarks[joints.right_shoulder].y,
+        ],
       };
       var mid_joint = {
         name: "right_elbow",
-        coord: [results.poseLandmarks[joints.right_elbow].x, results.poseLandmarks[joints.right_elbow].y]
+        coord: [
+          results.poseLandmarks[joints.right_elbow].x,
+          results.poseLandmarks[joints.right_elbow].y,
+        ],
       };
       var end_joint = {
         name: "right_wrist",
-        coord: [results.poseLandmarks[joints.right_wrist].x, results.poseLandmarks[joints.right_wrist].y]
+        coord: [
+          results.poseLandmarks[joints.right_wrist].x,
+          results.poseLandmarks[joints.right_wrist].y,
+        ],
       };
 
-      var angle = calculateAngles(first_joint.coord, mid_joint.coord, end_joint.coord);
+      var angle = calculateAngles(
+        first_joint.coord,
+        mid_joint.coord,
+        end_joint.coord
+      );
 
-      canvasCtx.fillText(mid_joint.name + " " + angle, mid_joint.coord[0] * width, mid_joint.coord[1] * height);
+      canvasCtx.fillText(
+        mid_joint.name + " " + angle,
+        mid_joint.coord[0] * width,
+        mid_joint.coord[1] * height
+      );
 
       var high = 160;
       var low = 30;
@@ -164,164 +171,121 @@ const getDeadTime = () => {
       if (angle < low && mode == false) {
         count += 1;
         mode = true;
-        
       }
-       
+
       canvasCtx.fillText(count, 200, 50);
-      if (count<adhere/2){
-        message = "keep going"
+      if (count < adhere / 2) {
+        message = "keep going";
+      } else if (count === adhere / 2) {
+        message = "Doing well!";
+      } else if (count === adhere) {
+        message = "EXCELLENT!";
+      } else if (count < adhere) {
+        message = "Very good!";
       }
-      else if (count===adhere/2){
-        message = "Doing well!"
-      }
-      else if (count===adhere){
-        message = "EXCELLENT!"
-      }
-      else if (count<adhere){
-        message = "Very good!"
-      }
-
-
-      
-
-    }
-    else
-      console.log("no detections");
-
-
+    } else console.log("no detections");
   }
 
   function onResults(results) {
-
-    canvasRef.current.width = webcamRef.current.video.videoWidth
-    canvasRef.current.height = webcamRef.current.video.videoHeight
+    canvasRef.current.width = webcamRef.current.video.videoWidth;
+    canvasRef.current.height = webcamRef.current.video.videoHeight;
     const canvasElement = canvasRef.current;
-    const canvasCtx = canvasElement.getContext("2d")
-    // End 
+    const canvasCtx = canvasElement.getContext("2d");
+    // End
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(results.image,
+    canvasCtx.drawImage(
+      results.image,
       0,
       0,
       canvasElement.width,
       canvasElement.height
-    )
-    drawConnectors(canvasCtx,
-      results.poseLandmarks, POSE_CONNECTIONS,
-      { color: '#FFFFFF', lineWidth: 2 });
-    // The dots are the landmarks 
-    drawLandmarks(canvasCtx, results.poseLandmarks,
-      { color: '#FFFFFF', lineWidth: 2, radius: 2 });
-    drawLandmarks(canvasCtx, results.poseWorldLandmarks,
-      { color: '#FFFFFF', lineWidth: 2, radius: 2 });
+    );
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+      color: "#FFFFFF",
+      lineWidth: 2,
+    });
+    // The dots are the landmarks
+    drawLandmarks(canvasCtx, results.poseLandmarks, {
+      color: "#FFFFFF",
+      lineWidth: 2,
+      radius: 2,
+    });
+    drawLandmarks(canvasCtx, results.poseWorldLandmarks, {
+      color: "#FFFFFF",
+      lineWidth: 2,
+      radius: 2,
+    });
 
-    poseEstimation(results)
+    poseEstimation(results);
     canvasCtx.restore();
-
   }
 
   useEffect(() => {
-
     clearTimer(getDeadTime());
-    
+
     const pose = new Pose({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-      }
+      },
     });
-
-   
 
     pose.setOptions({
       modelComplexity: 1,
       smoothLandmarks: true,
       minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
+      minTrackingConfidence: 0.5,
     });
 
-    pose.onResults(onResults)
-    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null) {
+    pose.onResults(onResults);
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
       camera = new cam.Camera(webcamRef.current.video, {
         onFrame: async () => {
-          await pose.send({ image: webcamRef.current.video })
+          await pose.send({ image: webcamRef.current.video });
         },
         width: 800,
-        height: 800
+        height: 800,
       });
-      camera.start()
+      camera.start();
     }
-    
-	// Another way to call the clearTimer() to start
-	// the countdown is via action event from the
-	// button first we create function to be called
-	// by the button
-	
-    
-  },[]);
+
+    // Another way to call the clearTimer() to start
+    // the countdown is via action event from the
+    // button first we create function to be called
+    // by the button
+  }, []);
 
   const onClickReset = () => {
     clearTimer(getDeadTime());
-}
-if(timer == '00:00:00'){
-   
-    
-    console.log(count)
-}
+    count = 0;
+  };
+  if (timer == "00:00:00") {
+    console.log(count);
+  }
 
   return (
     <div>
-      <div>
+      <div className="mt-5">
         <div class="row">
           <div class="col-md-6">
-          <div className="App">
-			    <h2>{timer}</h2>
-			    <button onClick={onClickReset}>Reset</button>
-
+            <div className="App">
+              <h2>{timer}</h2>
+              <button className="btn btn-success" onClick={onClickReset}>
+                Reset
+              </button>
+            </div>
+            <div class="text-danger font-weight-bold display-6"><p>{message}</p></div>
           </div>
-          <div class="card-desc">
-                        {message}
-                    </div>
-
-        </div>
-        <div class="col-md-6">
-          <div>
-          <Doughnut data={data} options={options}/>
+          <div class="col-md-6">
+            <div>
+              <Doughnut data={data} options={options} />
+            </div>
           </div>
-         
         </div>
-        
-      
-		</div>
-                    
-        
-        <div className='card-body'>
-          <Webcam
-            ref={webcamRef}
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 400,
-              height: 400,
-              marginBottom: "0px",
-            }} />
-          <canvas
-            ref={canvasRef}
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 400,
-              height: 400,
-              marginBottom: "0px"
-            }}>
-          </canvas>
-        </div>
-        
+          <CanvasWebCam webcamRef={webcamRef} canvasRef={canvasRef}/>
       </div>
 
       <br></br>
@@ -329,7 +293,7 @@ if(timer == '00:00:00'){
       <br></br>
       <p>{count}</p>
     </div>
-  )
-}
+  );
+};
 
-export default RightHandExtension
+export default RightHandExtension;
