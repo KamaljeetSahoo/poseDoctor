752efbdc93f818b4c1ab797b8902f0a8a69e90b6
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-03-16 17:22:20 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.scss'
+import './App.css'
 import Squats from "./components/Squats";
 import Home from "./components/Home";
 import HandExtension from "./components/HandExtension";
