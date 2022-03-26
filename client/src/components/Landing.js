f32b3e0e4d422c22fd645f3371f1a8dd3b51f4a0
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-03-26 20:20:08 +0530

    UI fixes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card, Col, Row } from "react-bootstrap";
 import "./landing.css";
 import pd from "../images/pd.png";
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -11,17 +11,21 @@ const Landing = () => {
           <img src={pd} className="img-fluid"></img>
         </div>
         <div className="col-md-6">
-          <div className="card-transparent pr-5">
+          <div className="card-transparent pr-5 pl-5">
             <div class="card-header text-center font-weight-bold">
               <i class="fa-solid fa-user-doctor landing_icon"></i> WELCOME!
             </div>
             <div class="card-block text-center d-flex justify-content-between">
                 <div></div>
               <button className="btn bg-transparent btn-xl mt-4 mb-4 justify-content-center shadow-lg">
-                SIGNUP
+              <Link to="/register" style={{textDecoration:'none'}} className="text-white">
+                    SIGN UP
+                </Link>
               </button>
               <button className="btn bg-transparent btn-xl mt-4 mb-4 justify-content-center shadow-lg">
-                LOGIN
+                <Link to="/login" style={{textDecoration:'none'}} className="text-white">
+                    LOGIN
+                </Link>
               </button>
               <div></div>
             </div>
