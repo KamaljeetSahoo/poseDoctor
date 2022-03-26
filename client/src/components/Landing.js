260108272867560bc0fe5e8f18e4a9434f592402
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-03-26 17:02:52 +0530

    landing fixed

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,38 +1,40 @@
-import React from 'react'
+import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import "./landing.css";
 import pd from "../images/pd.png";
 
 const Landing = () => {
   return (
-    <div className='container-landingpage bg-dark2 mb-3'>
-        <div className='col'>
-          <img src={pd} className="img-fluid">
-
-          </img>
+    <div className="container bg-dark2 mb-3">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={pd} className="img-fluid"></img>
         </div>
-        <div className='col'>
-        <div className="card-transparent">
-          <div class="card-header text-center font-weight-bold"><i class="fa-solid fa-user-doctor landing_icon"></i>  WELCOME!</div>
-          <div class="card-block">
-          <button className="btn bg-transparent btn-xl mt-4 mb-4 justify-content-center">
-                    SIGNUP
-          </button>
-          <button className="btn bg-transparent btn-xl mt-4 mb-4 justify-content-center">
-            LOGIN
-          </button>
+        <div className="col-md-6">
+          <div className="card-transparent pr-5">
+            <div class="card-header text-center font-weight-bold">
+              <i class="fa-solid fa-user-doctor landing_icon"></i> WELCOME!
+            </div>
+            <div class="card-block text-center d-flex justify-content-between">
+                <div></div>
+              <button className="btn bg-transparent btn-xl mt-4 mb-4 justify-content-center shadow-lg">
+                SIGNUP
+              </button>
+              <button className="btn bg-transparent btn-xl mt-4 mb-4 justify-content-center shadow-lg">
+                LOGIN
+              </button>
+              <div></div>
+            </div>
+            <div class="card-footer">
+              <i class="fa-brands fa-instagram landing_icon"></i>
+              <i class="fa-brands fa-facebook landing_icon"></i>
+              <i class="fa-brands fa-github landing_icon"></i>
+            </div>
           </div>
-          <div class="card-footer">
-            <i class="fa-brands fa-instagram landing_icon"></i>
-            <i class="fa-brands fa-facebook landing_icon"></i>
-            <i class="fa-brands fa-github landing_icon"></i>
-          </div>
         </div>
-        </div>
-        
+      </div>
     </div>
   );
-  
-}
+};
 
-export default Landing
+export default Landing;
